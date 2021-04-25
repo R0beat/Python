0b5fca7992d527c9684fd93cbb22846398a4f702
Roberto González Arriaga <htk.rgl@gmail.com>
2021-04-25 14:00:42 -0500

    Cadenas

---
+++ Caracteres/caracteres.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 13:48:09 2021
+
+@author: González
+"""
+#%% Cadenas son secuencias de caracteres dentro de comillas
+nombre = 'Roberto Gonzalez Arriaga'
+carrera = 'L.I.A.'
+
+type(nombre)
+
+print(carrera,nombre)
+#%% print() esta funcion nos permite mostrar por pantalla texto
+print('Esta cadena esta \t separada por tabulación')
+print('Esta cadena esta \n separada por un salto de linea')
+# Para que la funcion print nos muestre una cadena sin procesas usamos r
+print(r'C:\Users\González\Documents\Python')
+# Texto en varias lineas
+print('''Neque porro quisquam
+      est qui dolorem ipsum quia 
+      dolor sit amet, consectetur, 
+      adipisci velit...''')

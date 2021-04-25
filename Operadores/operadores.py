85cd935c0f0cf3c5a8e7c368a5b024ad4ae26b07
Roberto González Arriaga <htk.rgl@gmail.com>
2021-04-25 16:23:29 -0500

    Operadores

---
+++ Operadores/operadores.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 16:07:30 2021
+
+@author: González
+"""
+
+# Adición
+8 + 30.5
+7 + 7
+
+# Substracción
+10 - 5
+
+# Multiplicación
+3.1 * 3.2
+'A' * 6 
+'A' + 'B'
+
+# División 
+10 / 5
+# División entera tuncada
+10 // 5
+# División residuo
+5 % 2
+# Exponenciación 
+4 ** 5
+
+# Operadores de asignación
+edad = 10 
+print(edad)
+
+edad = edad - 3
+print(edad)
+
+edad -= 3
+print(edad)
+
+edad /=5
+print(edad)
+
+edad //= 2
+print(edad)
+
+edad *= 2
+print(edad)
+
+edad **=3
+print(edad)
+

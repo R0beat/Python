b81ebe4e38dc4f850b28a3be2e5cc9ff723b3505
Roberto González Arriaga <htk.rgl@gmail.com>
2021-04-25 14:23:15 -0500

    'Booleanos'

---
+++ Booleanos/booleano.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 14:03:11 2021
+
+@author: González
+"""
+
+#Booleanos solo tienen 2 categorias verdadero o falso
+ 
+suma = 1 + 1
+
+suma == 2
+
+suma == 3
+
+booleano = True
